b11d79c66c5cbd50e76249afcf1b3b64696f4e77
Syed burhan <syedburhanali555@gmail.com>
2022-03-02 01:30:14 +0500

    make it better

--- app.js
+++ app.js
@@ -25,11 +25,17 @@ app.use("/api/orders", OrderRoutes);
 app.use("/api/checkouts", stripeRoutes);
 
 
-app.use(express.static(path.join(__dirname, "mern", "build")))
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "mern", "build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "mern", "build")))
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "mern", "build", "index.html"));
+// });
 
+if(process.env.NODE_ENV === 'production'){    
+  app.use(express.static(path.join(__dirname, "client", "build")))
+  app.get('*',(req,res)=>{     
+      res.sendFile (path.resolve(__dirname,'client','build','index.html' ));    
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Connection is setup on ${PORT}`);
